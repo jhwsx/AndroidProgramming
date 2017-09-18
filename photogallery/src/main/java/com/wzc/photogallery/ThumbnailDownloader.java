b5343d3a352f5abdb23148aeca004a91f0b59bfb
wangzhichao <392337950@qq.com>
2017-09-18 08:18:30 +0800

    1,结束第24章:挑战练习未完成(预加载及缓存)

--- photogallery/src/main/java/com/wzc/photogallery/ThumbnailDownloader.java
+++ photogallery/src/main/java/com/wzc/photogallery/ThumbnailDownloader.java
@@ -20,6 +20,9 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     private static final int MESSAGE_DOWNLOAD = 0;
     private Boolean mHasQuit = false;
     private Handler mRequestHandler;
+    // 线程安全的HashMap,使用标记下载请求的T类型对象作为key,使用和请求关联的url下载链接作为value,
+    // 这样的目的是为了将下载结果和需要显示图片的UI元素关联起来,也就是说,可以清楚地知道将下载结果
+    // 发送给哪个显示图片的UI元素.
     private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
     // 存放来自主线程的Handler
     private Handler mResponseHandler;
@@ -29,18 +32,24 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     }
 
     private ThumbnailDownloadListener<T> mThumbnailDownloadListener;
+
     public interface ThumbnailDownloadListener<T> {
         void onThumbnailDownloaded(T target, Bitmap thumbnail);
     }
+
     public ThumbnailDownloader(Handler responseHandler) {
         super(TAG);
         mResponseHandler = responseHandler;
     }
-
+    /**
+     * Call back method that can be explicitly overridden if needed to execute some
+     * setup before Looper loops.
+     * 在Looper首次检查消息队列之前调用,这里是创建Handler的好地方
+     */
     @Override
     protected void onLooperPrepared() {
         super.onLooperPrepared();
-        mRequestHandler = new Handler(){
+        mRequestHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
@@ -56,6 +65,7 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     public void clearQueue() {
         mRequestHandler.removeMessages(MESSAGE_DOWNLOAD);
     }
+
     private void handleRequest(final T target) {
         final String url = mRequestMap.get(target);
 
@@ -75,7 +85,7 @@ public class ThumbnailDownloader<T> extends HandlerThread {
                         return;
                     }
                     mRequestMap.remove(target);
-                    mThumbnailDownloadListener.onThumbnailDownloaded(target,bitmap);
+                    mThumbnailDownloadListener.onThumbnailDownloaded(target, bitmap);
                 }
             });
         } catch (IOException e) {
@@ -90,13 +100,18 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     }
 
     public void queueThumbnail(T target, String url) {
-        Log.i(TAG,"Got a url: " + url);
+        Log.i(TAG, "Got a url: " + url);
 
         if (url == null) {
             mRequestMap.remove(target);
         } else {
             mRequestMap.put(target, url);
-            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD,target).sendToTarget();
+            // 新消息代表的是什么?是T target(RecyclerView中的PhotoHolder的下载请求)
+            // 注意,消息自身是不带url信息的.
+            // 把PhotoHolder和url的对应关系更新到mRequestMap里面,随后,从mRequestMap中取出图片url,以保证总是使用了匹配
+            // PhotoHolder实例的最新下载请求url(因为RecyclerView中的PhotoHolder是会不断回收复用的)
+            // TODO 思考,如果不这样做,还有别的办法吗?
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
         }
     }
 }
